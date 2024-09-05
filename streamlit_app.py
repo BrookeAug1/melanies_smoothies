2e9efd2d757d837566a7d268efe5f158c9fcf76c
BrookeAug1 <44063647+BrookeAug1@users.noreply.github.com>
2024-09-05 13:57:02 -0400

    Update streamlit_app.py
    Search selected fruit- basic

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit)
         fv_df = st.dataframe(data=fruityvice_response.json())
 
     my_insert_statement = """insert into smoothies.public.orders(ingredients, name_on_order) values 
